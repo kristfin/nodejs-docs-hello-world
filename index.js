9484faf21a3972548c210f01abb22939e196abed
Kristján Þór Finnsson <kfinnsson@ossur.com>
2020-04-15 14:30:50 +0000

    Added log to req

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const http = require('http');
+const port = process.env.PORT || 1337;
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
+    console.log("Hello world, we are on port "+port);
 });
 
-const port = process.env.PORT || 1337;
+
 server.listen(port);
+
+console.log("Jabadabadu");
 
 console.log("Server running at http://localhost:%d", port);
