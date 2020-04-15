c3718b7b46e5d6f801e7959684bebd0b333ae2de
Kristján Þór Finnsson <kfinnsson@ossur.com>
2020-04-15 14:44:36 +0000

    Added node-red

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const http = require('http');
-const port = process.env.PORT || 1337;
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-    console.log("Hello world, we are on port "+port);
+const port = process.env.PORT || 1337;
+console.log("Using port "+port);
+
+// Create an Express app
+var app = express();
+
+// Add a simple route for static content served from 'public'
+app.use("/",express.static("public"));
+
+app.get('/debug', function (req, res) {
+    console.log("Somebody asking for debug, yay!");
+    console.log("we are listening to port "+port);
+    res.send("we are listening to port "+port)
 });
 
+// Create a server
+var server = http.createServer(app);
+
+// Create the settings object - see default settings.js file for other options
+var settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"./.node-red/",
+    functionGlobalContext: { }    // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
 
 server.listen(port);
 
-console.log("Jabadabadu");
-
-console.log("Server running at http://localhost:%d", port);
+// Start the runtime
+RED.start();
